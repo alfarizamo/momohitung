974aaf7bae2911daa0434da05fec20309e0dfc1d
alfarizamo <160070893+alfarizamo@users.noreply.github.com>
2024-09-23 09:48:48 +0700

    Update momohitung.py

--- momohitung.py
+++ momohitung.py
@@ -1,7 +1,7 @@
 impor streamlit as st
 
 x = st.number_input(
-  "Insert a number"' value=None, placeholder="Type a number..."
+  "Insert a number", value=None, placeholder="Type a number..."
 )
 st.write("The current number is ", x)
 st.latex(r'''
